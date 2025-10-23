0fdb99805bc247b830e6132d5958f24b8ce13360
Arjan Anant <arjananant2@gmail.com>
2025-10-23 11:37:01 +0000

    404 File not found
    The site configured at this address does not contain

--- next.config.js
+++ next.config.js
@@ -31,19 +31,6 @@ const nextConfig = {
       }
     ],
   },
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: "script-src 'self' 'unsafe-eval' 'unsafe-inline'; worker-src blob:;",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
