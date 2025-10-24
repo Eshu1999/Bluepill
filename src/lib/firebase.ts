7918dc9ec46720f4208febec5ccdfdefe10561c6
Arjan Anant <arjananant2@gmail.com>
2025-10-24 04:45:31 +0000

    That's an Google login error and it is not fixed

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,13 @@ import { getAuth, GoogleAuthProvider, sendEmailVerification, signInWithPopup, ge
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  "projectId": process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  "appId": process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  "apiKey": process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  "authDomain": process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  "measurementId": process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-  "messagingSenderId": process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  "storageBucket": process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREbase_MEASUREMENT_ID
 };
 
 
